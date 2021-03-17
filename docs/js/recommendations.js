77b4222d80efd358acf2d6b8f97f37b09873bda8
josiahmokob0 <jmnyarega@gmail.com>
2021-03-17 05:58:39 +0300

    recommendations from recommendation.json

--- docs/js/recommendations.js
+++ docs/js/recommendations.js
@@ -18,24 +18,24 @@ new Vue({
             return results
         }
     },
-    template:`
-      <div class="ui cards fluid" v-for="recommendation in recommendations">
-        {{ recommendation }}
-        <div class="segment">
-          <div class="content" id="recommendations">
-            <span class="ui teal ribbon label">Stem Disintermedia</span>
-            <div class="content">
-              <div class="header center">Jay</div>
-              <div class="meta">
-                <span class="category center">Stem</span>
-              </div>
-              <div class="description">
-                <blockquote>
-                Josiah was part of a 3 person team developing full-stack React/Node/Postgres applications. I challenged the team to learn d3, SVG, CSS, and HTML5 in a deeper way, and to switch from GraphQL to using Django and a REST interface. Josiah dug in and became familiar with the d3 way which is very different from a React component approach. His ability to face challenges directly and overcome them, plus an ability to learn new technologies quickly makes him a strong asset for any team. I would gladly work with him again.
-              </blockquote>
+    template: `
+      <div>
+        <div class="ui cards fluid" v-for="recommendation in recommendations">
+          <div class="ui card fluid">
+            <div class="ui raised segment">
+              <div class="segment">
+                <div class="content" id="recommendations">
+                  <span class="ui teal ribbon label">{{ recommendation["worked_together"] }}</span>
+                  <div class="content">
+                    <div class="header center">{{ recommendation["First Name"] }} {{ recommendation["Last Name"] }}</div>
+                    <div class="description">
+                      <blockquote> {{ recommendation.Text }}</blockquote>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            </div>
+          </div>
         </div>
       </div>
     `
