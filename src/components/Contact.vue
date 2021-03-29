7e6dcb367e5015eb5ebc1efdfe9a1f57afda19de
josiahmokob0 <jmnyarega@gmail.com>
2021-03-29 18:47:50 +0300

    - install vue package & integrate - form submit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,37 +1,101 @@
-<template>
-
+<template lang="html">
   <div id="contact">
     <h2 class="center heading">Get in Touch</h2>
     <div id="contact-form">
-      <form class="ui form">
-        <div class="field">
-          <label>Subject:
-            <input type="text" name="_subject" placeholder="Subject">
-          </label>
+      <sui-form>
+        <sui-form-field>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="email"
+            v-model="email"
+          />
+        </sui-form-field>
+        <sui-form-field>
+          <label>Subject</label>
+          <input
+            type="text"
+            placeholder="First Name"
+            subject="subject"
+            v-model="subject"
+            />
+        </sui-form-field>
+        <sui-form-field>
+          <label>Short Text</label>
+          <textarea
+            rows="2"
+            name="content"
+            v-model="content"
+            />
+            <sui-button
+              positive
+              @click="submit"
+              v-bind:loading="loading">
+              Submit
+            </sui-button>
+        </sui-form-field>
+
+        <div class="feedback">
+          <transition name="fade">
+          <sui-message
+            v-if="message"
+            header="Welcome back!"
+            dismissable
+            @dismiss="handleDismiss">
+            {{message}}
+          </sui-message>
+          </transition>
         </div>
-        <div class="field">
-          <label>Email:
-            <input type="email" name="_replyto" placeholder="Email">
-          </label>
-        </div>
-        <div class="field">
-          <label>Message:
-            <textarea name="message"></textarea>
-          </label>
-        </div>
-        <button class="ui button teal large" type="submit">Submit</button>
-      </form>
-      <div class="ui success message transition response">
-        <i class="close icon"></i>
-        <div class="header">
-          Success 😼
-        </div>
-        <p id="response_text"></p>
-      </div>
+      </sui-form>
     </div>
   </div>
 
 </template>
+
+<script type = "text/javascript">
+
+export default  {
+  name: "Contact",
+  data() {
+    return {
+      email: "",
+      subject: "",
+      content: "",
+      message: "",
+      loading: false
+    };
+  },
+  methods : {
+    submit : async function(evt) {
+      evt.preventDefault();
+      const {content, subject, email} = this
+      const dt = { content, subject, email };
+      await this.sendForm(dt);
+    },
+    async sendForm(data) {
+      try {
+        this.loading = true;
+        await fetch("https://formspree.io/f/xknpaepv", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        this.loading = false;
+        this.message = "Your message was successfully sent, I will get back to you soonest.";
+      } catch (error) {
+        this.loading = false;
+        this.message = "Something went wrong"
+      }
+    },
+    handleDismiss() {
+      this.message = "";
+    }
+  },
+}
+</script>
 
 <style scoped>
 #contact {
@@ -56,5 +120,8 @@
 #contact-form textarea {
   height: 150px;
   resize: none;
+}
+.feedback {
+  margin: 2rem 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
+import SuiVue from 'semantic-ui-vue';
+
 import App from "./App.vue";
+
+Vue.use(SuiVue);
 
 new Vue({
   render: (h) => h(App),
