6521ccd5001a04b649e368d2ef5d261d20015835
josiahmokob0 <jmnyarega@gmail.com>
2021-03-31 20:52:00 +0300

    un-bloat

--- portfolio/src/components/container.js
+++ portfolio/src/components/container.js
@@ -1,12 +1,8 @@
 import styled from "styled-components";
-import { theme } from "../styles";
 
 export const Container = styled.div`
   max-width: 18.75rem;
   margin: 0 auto;
 `;
 
-export const Main = styled.div`
-  background-color: ${theme.background};
-  color: red;
-`;
+export const Main = styled.div``;
